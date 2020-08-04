3403bf5852b1c5a4a35d03393d92e435c7740fa4
Nitzan Listman <67858069+listguy@users.noreply.github.com>
2020-08-04 20:51:31 +0300

    added comments and search in arraymethod

--- rest.js
+++ rest.js
@@ -20,11 +20,15 @@ let products = [
 ];
 
 //'get' entry point to recive all products
+// get: nothing
+// return: products array
 app.get('/products', (req, res) => {
     res.send(products);
 });
 
 // 'get' entry point to recive an item by it's id
+// get: an id
+// return: product object
 app.get('/products/:productID', (req, res) => {
     products.forEach(product => {
         if(product.id === req.params.productID) {
@@ -34,32 +38,50 @@ app.get('/products/:productID', (req, res) => {
 });
 
 // 'post' entry point to add a new product to the list
+// get: json object with one key- product, and a string value(the product you want to add)
+// return: new product object
 app.post('/products', (req, res) => {
     let newProduct = {
-        id: products.length + 1,
+        id: parseInt(products[products.length -1].id) + 1+"",
         product: req.body.product
     };
     products.push(newProduct);
     res.send(newProduct);
 })
 
+// 'put' entry point to update an existing product by id.
+// get: a new product object 
+// return: the updated product object
 app.put('/products/:productID', (req, res) => {
-    products.forEach((product, i) => {
-        if(product.id === req.params.productID) {
-            products[i] = req.body;
-            res.send(req.body);
-        }
-    });
+    let index = FindIndexOfProduct(req.params.productID);
+    if(index != undefined) {
+        products[index].product = req.body.product;
+        res.send(products[index]);
+    }
 });
 
+// 'delete' entry point to delete an exsisting product by id.
+// get: an id
+// return: undefined
 app.delete('/products/:productID', (req, res) => {
-    products.forEach((product, i) => {
-        if(product.id === req.params.productID) {
-            products.splice(i, 1);
-            res.send();
-        }
-    });
+    let index = FindIndexOfProduct(req.params.productID);
+    if(index != undefined) {
+        products.splice(index, 1);
+    }
+    res.send();
 });
 
+// method finds and returns the index of the object with id of rpId
+// get: an id
+// return: index of object if it succeed, undefined else  
+function FindIndexOfProduct(rpId) {
+    let index;
+    products.forEach((product, i) => {
+        if(product.id === rpId) {
+            index = i;
+        }
+    });
+    return index;
+};
 
 app.listen(3000);
